77a0433fd9adf4b53e3d051c1c352da19764274a
shiva1575 <34787199+shiva1575@users.noreply.github.com>
2018-03-08 15:36:46 +0530

    Update create-containers.sh

--- create-containers.sh
+++ create-containers.sh
@@ -7,7 +7,7 @@ do
 	echo "=============================="
     echo "Creating www.gamutkart$i container.."
     sleep 1
-    docker run --name www.gamutkart$i -it jenkins /bin/bash
+    docker run --name www.gamutkart$i -d -it jenkins /bin/bash
     echo "www.gamutkart$i container has been created!"
 	echo "=============================="
 done
